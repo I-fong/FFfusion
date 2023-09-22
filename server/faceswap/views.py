9e712301954aeccb3c14d3424af76eca7886e48a
dh5473 <dh5473@naver.com>
2023-09-23 01:20:02 +0900

    Refactor ImageUpload API

--- server/faceswap/views.py
+++ server/faceswap/views.py
@@ -1,17 +1,14 @@
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 from django.shortcuts import render, redirect
+from rest_framework.views import APIView
 from django.urls import reverse
-from django.http import HttpResponse
+
 from .forms import SimpleUploadForm, ImageUploadForm
 from .models import ImageUploadModel
-from django.core.files.storage import FileSystemStorage
-from django.conf import settings
 from .utils import cv_swap_face, face_bbox
-from .apps import FaceswapConfig
-from PIL import Image
-import numpy as np
-import json
-import pickle
-from rest_framework.views import APIView
+
+import requests
 
 
 class ImageUpload(APIView):
@@ -22,23 +19,23 @@ class ImageUpload(APIView):
     def post(self, request):
         form = ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            post = form.save(commit=False)            
-            target_image = request.FILES['target_image']
+            post = form.save(commit=False)
             source_image = request.FILES['source_image']
-            post.target_image = target_image
+
             post.source_image = source_image
-            ###
-            post.save()  # DB에 실제로 form 객체('form')에 채워져 있는 데이터를 저장
+            post.save()
 
-            target_url = settings.MEDIA_URL + form.instance.target_image.name
-            source_url = settings.MEDIA_URL + form.instance.source_image.name
-            # 실제로 db에 저장되어있는 경로 '/media/images/2021/07/30/image.jpg'
+            target_image_s3_url = request.POST.get('target_image')
+            target_image_content = requests.get(target_image_s3_url).content
+            target_image_name = target_image_s3_url.split('/')[-1]
+            target_image_path = f"target_images/{target_image_name}"  # 저장될 경로 설정
+            default_storage.save(target_image_path, ContentFile(target_image_content))
 
-            # 저장된 이미지를 가져오기
             upload_image_model = ImageUploadModel.objects.create(
-                target_image=target_image, source_image=source_image
-                )
-            # reverse 함수를 사용하여 URL을 생성하고 해당 URL로 리다이렉트
+                                                                target_image=target_image_path,
+                                                                source_image=source_image
+                                                                )
+
         return redirect(reverse('faceswap:swap_face', kwargs={'id': upload_image_model.id}))
 
 
